234e72875260d33c163bed4e1ea569f27a092ac1
bryxnn <85969812+bryxnn@users.noreply.github.com>
2023-04-20 14:28:44 -0400

    final review, 7 questions

---
+++ final.java
@@ -0,0 +1,15 @@
+5.5 predict output main class and recurssive method
+
+5.5 fibonacci sequence with recurssive method, HOW MANY TIMES DOES IT CALL IT 4, 8, 12, 14
+
+5.5 binary search, given the following list of sorted elements, how many elemnts of the list would be 
+
+3 stack data structure true or false
+
+3 asks stacked or queue
+
+5.5 predict the outcome of the following program, a stack implementation in the main and some push and pop from stack
+
+5.5 given one linked list of a pet object, which code can be used to find and remove that pet from link list
+study how to remove something from link list
+
